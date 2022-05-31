73332c7387a80e1d635a2915fe5858a0094c9424
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-05-31 14:05:25 -0300

    .

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
+import Aleatorio from './components/basicos/Aleatorio'
 
-export default () =>
+export default () => (
     <div id='app'>
         <h1>Fundamentos React</h1>
+        <Aleatorio
+            valMin = {70}
+            valMax = {539}/>
+
         <Fragmento />
 
         <ComParametro
@@ -16,3 +21,4 @@ export default () =>
 
         <Primeiro />
     </div>
+);
---
+++ fundamentos-react/src/components/basicos/Aleatorio.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Match } from '@testing-library/react'
+
+export default (props) => {
+    const valMin = props.valMin
+    const valMax = props.valMax
+    const numAleatorio = Math.floor(Math.random() * (valMax - valMin + 1)) +  valMin
+
+    return (
+        <>
+            <h2>Desafio Aleatorio</h2>
+            <p>Recebi dois numeros: {valMin}(Minimo) e {valMax}(Maximo)</p>
+            <p>O numero aleatorio derado entre esses dois foi: {numAleatorio}</p>
+        </>
+    )
+}
