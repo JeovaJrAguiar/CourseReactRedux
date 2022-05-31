dbaf8f203dcf6a02774f82e1244d90b8ddd972a0
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-05-31 13:41:23 -0300

    .

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -4,11 +4,22 @@ import './index.css'
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 ReactDOM.render(
     <div>
         <Primeiro/>
-        <ComParametro titulo='Titulo do Parametro' subTitulo='SubTitulo do parametro'/>
+        <ComParametro 
+            titulo='Situaçao do aluno:' 
+            aluno='Carlos'
+            nota={1.9}/>
+
+        <ComParametro 
+            titulo='Situaçao do aluno:' 
+            aluno='Maria'
+            nota={9.9}/>
+
+        <Fragmento/>
     </div>, 
     document.getElementById('root')
 )
