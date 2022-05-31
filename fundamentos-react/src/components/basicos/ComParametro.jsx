cd2b599f2a88c597c118104098ccdc79d959dc6a
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-05-30 23:38:46 -0300

    .

--- fundamentos-react/src/components/basicos/ComParametro.jsx
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -1,10 +1,10 @@
 import React from 'react-dom'
 
-export default function ComParametro(params){
+export default function ComParametro(props){
     return (
         <div>
-            <h2>{params.titulo}</h2>
-            <h3>{params.subTitulo}</h3>
+            <h2>{props.titulo}</h2>
+            <h3>{props.subTitulo}</h3>
         </div>
     )
 }
