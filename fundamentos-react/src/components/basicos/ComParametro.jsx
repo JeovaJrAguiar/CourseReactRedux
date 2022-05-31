fa10ff337fbc0ee0c41437d68fbe6a28e388f688
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-05-30 23:38:08 -0300

    .

--- fundamentos-react/src/components/basicos/ComParametro.jsx
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -1,1 +1,10 @@
 import React from 'react-dom'
+
+export default function ComParametro(params){
+    return (
+        <div>
+            <h2>{params.titulo}</h2>
+            <h3>{params.subTitulo}</h3>
+        </div>
+    )
+}
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,10 +3,12 @@ import React from 'react' // para usar JSX
 import './index.css'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
     <div>
-        <Primeiro></Primeiro>
+        <Primeiro/>
+        <ComParametro titulo='Titulo do Parametro' subTitulo='SubTitulo do parametro'/>
     </div>, 
     document.getElementById('root')
 )
